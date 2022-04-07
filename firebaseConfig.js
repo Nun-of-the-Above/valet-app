0b4f5f4837bc53b44ae5ef2aa694d5ccf8a51124
NunOfTheAbove <85111869+Nun-of-the-Above@users.noreply.github.com>
2022-04-07 15:35:24 +0200

    Moved config to seperate file

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBt9-jaNbY3Z98P_YZJmGEW56xSBb89TKA",
+  authDomain: "valet-app-2ab35.firebaseapp.com",
+  projectId: "valet-app-2ab35",
+  databaseURL:
+    "https://valet-app-2ab35-default-rtdb.europe-west1.firebasedatabase.app",
+  storageBucket: "valet-app-2ab35.appspot.com",
+  messagingSenderId: "363053276470",
+  appId: "1:363053276470:web:1cf479399af2286b366797",
+  measurementId: "G-F39P75W8Z7",
+};
--- src/firestore.js
+++ src/firestore.js
@@ -3,22 +3,12 @@ import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 import { getDatabase, connectDatabaseEmulator } from "firebase/database";
 import { debugToken } from "./debugToken";
+import { firebaseConfig } from "../firebaseConfig";
+
 const {
   initializeAppCheck,
   ReCaptchaV3Provider,
 } = require("firebase/app-check");
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBt9-jaNbY3Z98P_YZJmGEW56xSBb89TKA",
-  authDomain: "valet-app-2ab35.firebaseapp.com",
-  projectId: "valet-app-2ab35",
-  databaseURL:
-    "https://valet-app-2ab35-default-rtdb.europe-west1.firebasedatabase.app",
-  storageBucket: "valet-app-2ab35.appspot.com",
-  messagingSenderId: "363053276470",
-  appId: "1:363053276470:web:1cf479399af2286b366797",
-  measurementId: "G-F39P75W8Z7",
-};
 // eslint-disable-next-line no-restricted-globals
 self.FIREBASE_APPCHECK_DEBUG_TOKEN = debugToken;
 
